50bef6b1a36b66afc4474a878122118aa3ef7547
esfox <osairis100@gmail.com>
2023-04-25 00:15:06 +0800

    Remove unnecessary comment

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    -- local configuration = vim.fn['sonokai#get_configuration']()
-    -- local palette = vim.fn['sonokai#get_palette'](configuration.style, configuration.colors_override)
-    -- local custom_sonokai = require 'lualine.themes.sonokai'
-    -- custom_sonokai.normal.a.bg = palette.bg_green[1]
-    -- custom_sonokai.insert.a.bg = palette.bg_blue[1]
-
     local function show_macro_recording()
       local recording_register = vim.fn.reg_recording()
       if recording_register == "" then
