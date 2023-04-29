18ef7b8684c4e6d3dd481e708055f0f86a1dfd55
esfox <osairis100@gmail.com>
2023-04-29 19:41:11 +0800

    Update lsp servers setup config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -41,56 +41,55 @@ function lsp.setup()
 
   -- Ensure the servers above are installed
   local mason_lspconfig = require("mason-lspconfig")
-
   mason_lspconfig.setup({
     ensure_installed = vim.tbl_keys(servers),
   })
 
   mason_lspconfig.setup_handlers({
     function(server_name)
-      require("lspconfig")[server_name].setup({
+      local lspconfig = require("lspconfig")
+      local lsp_setup_config = {
         capabilities = capabilities,
         on_attach = on_attach,
         settings = servers[server_name],
-      })
-    end,
-  })
+      }
 
-  -- Enable Tailwind color highlighting
-  local lspconfig = require("lspconfig")
-  lspconfig.tailwindcss.setup({
-    on_attach = function(_, bufnr)
-      require("tailwindcss-colors").buf_attach(bufnr)
-    end,
-  })
+      if server_name == "tsserver" then
+        lsp_setup_config["handlers"] = {
+          ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
+            if result.diagnostics == nil then
+              return
+            end
 
-  lspconfig.tsserver.setup({
-    handlers = {
-      ["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
-        if result.diagnostics == nil then
-          return
+            -- ignore some tsserver diagnostics
+            local idx = 1
+            while idx <= #result.diagnostics do
+              local entry = result.diagnostics[idx]
+
+              local formatter = require("format-ts-errors")[entry.code]
+              entry.message = formatter and formatter(entry.message) or entry.message
+
+              -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+              if entry.code == 80001 then
+                -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+                table.remove(result.diagnostics, idx)
+              else
+                idx = idx + 1
+              end
+            end
+
+            vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+          end,
+        }
+      elseif server_name == "tailwindcss" then
+        -- Enable Tailwind color highlighting
+        lsp_setup_config["on_attach"] = function(_, bufnr)
+          require("tailwindcss-colors").buf_attach(bufnr)
         end
+      end
 
-        -- ignore some tsserver diagnostics
-        local idx = 1
-        while idx <= #result.diagnostics do
-          local entry = result.diagnostics[idx]
-
-          local formatter = require("format-ts-errors")[entry.code]
-          entry.message = formatter and formatter(entry.message) or entry.message
-
-          -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-          if entry.code == 80001 then
-            -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
-            table.remove(result.diagnostics, idx)
-          else
-            idx = idx + 1
-          end
-        end
-
-        vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-      end,
-    },
+      lspconfig[server_name].setup(lsp_setup_config)
+    end,
   })
 end
 
