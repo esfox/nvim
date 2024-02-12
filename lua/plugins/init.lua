409bc90e3d850399b70bba164f5b296865dc9367
esfox <osairis100@gmail.com>
2024-02-12 23:38:15 +0800

    Update plugins & try Harpoon

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,6 +61,15 @@ return {
   --   },
   -- },
   {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+    end,
+  },
+  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
