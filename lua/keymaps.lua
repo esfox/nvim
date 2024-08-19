9c137d778575e72f174b3301557209179acd29db
esfox <osairis100@gmail.com>
2024-08-19 17:31:16 +0800

    Show previewr in current buffer fuzzy find

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -221,10 +221,7 @@ function keymaps.for_plugins()
   vim.keymap.set("n", "<leader>/", function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
     require("telescope.builtin").current_buffer_fuzzy_find(
-      require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-      })
+      require("telescope.themes").get_dropdown()
     )
   end, { desc = "[/] Fuzzily search in current buffer" })
 
