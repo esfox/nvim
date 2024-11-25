1e5aab1269e71572ecfba0e419719a7f88baf118
esfox <osairis100@gmail.com>
2024-11-25 14:36:39 +0800

    Cleanup plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -319,83 +319,8 @@ return {
     opts = {},
   },
   {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      dap.adapters.coreclr = {
-        type = "executable",
-        command = "/home/ckftm/software/netcoredbg/netcoredbg",
-        args = { "--interpreter=vscode" },
-      }
-
-      dap.configurations.cs = {
-        {
-          type = "coreclr",
-          name = "launch - netcoredbg",
-          request = "launch",
-          program = function()
-            -- return vim.fn.DOTNET_DEBUG_DLL_PATH;
-            return vim.fn.input({
-              prompt = "Path to dll to debug: ",
-            })
-          end,
-        },
-      }
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.after.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.after.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "microsoft/vscode-js-debug",
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-  },
-  {
-    "mxsdev/nvim-dap-vscode-js",
-    config = function()
-      require("dap-vscode-js").setup({
-        debugger_path = "/home/ckftm/.local/share/nvim/lazy/vscode-js-debug",
-        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-        -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-      })
-
-      for _, language in ipairs({ "typescript", "javascript" }) do
-        require("dap").configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-        }
-      end
-    end,
+    "folke/zen-mode.nvim",
+    opts = {},
   },
   {
     "mhartington/formatter.nvim",
