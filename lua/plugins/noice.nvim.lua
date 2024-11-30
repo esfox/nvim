1cbc19ee4aab26652fcfadc88e82be1b5e68812d
esfox <osairis100@gmail.com>
2024-11-30 12:14:24 +0800

    Update noice messages view

--- lua/plugins/noice.nvim.lua
+++ lua/plugins/noice.nvim.lua
@@ -1,31 +1,46 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    lsp = {
-      progress = {
-        enabled = false,
-      },
-    },
-    cmdline = {
-      format = {
-        cmdline = {
-          view = "cmdline",
-          icon = ":",
-        },
-        search_down = { title = "" },
-        search_up = { title = "" },
-      },
-    },
-    views = {
-      cmdline_popup = {
-        border = {
-          style = "single",
-        },
-      },
-    },
-  },
   dependencies = {
     "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
   },
+  config = function()
+    require("notify").setup({
+      render = "minimal",
+      timeout = 3000,
+      top_down = false,
+      stages = "static",
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { border = "single" })
+      end,
+    })
+
+    require("noice").setup({
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+      },
+      cmdline = {
+        format = {
+          cmdline = {
+            view = "cmdline",
+            icon = ":",
+          },
+          search_down = { title = "" },
+          search_up = { title = "" },
+        },
+      },
+      views = {
+        cmdline_popup = {
+          border = {
+            style = "single",
+          },
+        },
+      },
+    })
+
+    vim.keymap.set("n", "<leader>sn", ":Telescope notify<cr>")
+  end,
 }
