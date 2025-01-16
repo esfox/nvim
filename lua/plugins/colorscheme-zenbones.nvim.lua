6050b3bc958bd31c6fa4fbacbbeccde91364c691
esfox <osairis100@gmail.com>
2025-01-16 18:36:05 +0800

    Add writer mode

---
+++ lua/plugins/colorscheme-zenbones.nvim.lua
@@ -0,0 +1,18 @@
+return {
+  "zenbones-theme/zenbones.nvim",
+  dependencies = "rktjmp/lush.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.zenbones_darken_comments = 45
+    -- vim.cmd.colorscheme('zenbones')
+
+    vim.api.nvim_create_user_command("WriterMode", function(_)
+      vim.cmd("set background=light")
+      vim.cmd("colorscheme seoulbones")
+      vim.cmd("set wrap")
+      vim.cmd("set cc=")
+      vim.opt.guifont = { "JetBrains Mono NL SemiBold", ":h17" }
+    end, { desc = "Set light colorscheme and word wrap" })
+  end,
+}
