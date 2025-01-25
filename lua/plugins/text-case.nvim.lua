0254d1cbca77245c1dad6ef1b894711cd4c4918d
esfox <osairis100@gmail.com>
2025-01-25 11:59:51 +0800

    Add text-case.nvim keymap

--- lua/plugins/text-case.nvim.lua
+++ lua/plugins/text-case.nvim.lua
@@ -4,5 +4,7 @@ return {
   config = function()
     require("textcase").setup({})
     require("telescope").load_extension("textcase")
+
+    vim.keymap.set({ "n", "v" }, "gt", "<cmd>TextCaseOpenTelescope<CR>")
   end,
 }
