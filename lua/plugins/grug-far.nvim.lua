448efa6f6e0c23097259df745852436457fea2fb
esfox <osairis100@gmail.com>
2025-09-24 14:18:37 +0800

    Update grug-far

--- lua/plugins/grug-far.nvim.lua
+++ lua/plugins/grug-far.nvim.lua
@@ -2,12 +2,13 @@ return {
   "MagicDuck/grug-far.nvim",
   version = "1.6.3",
   config = function()
-    require("grug-far").setup({
+    local grug_far = require("grug-far")
+    grug_far.setup({
       openTargetWindow = { preferredLocation = "above" },
       keymaps = {
         toggleShowCommand = { n = "" },
       },
-      windowCreationCommand = "split",
+      windowCreationCommand = "bo split",
       helpLine = {
         enabled = false,
       },
@@ -19,8 +20,26 @@ return {
     })
 
     vim.keymap.set("n", "<leader>lg", function()
-      vim.cmd("bo GrugFar")
+      grug_far.open()
 
+      -- vim.api.nvim_create_autocmd("FileType", {
+      --   group = vim.api.nvim_create_augroup("grug-far-keybindings", { clear = true }),
+      --   pattern = { "*Grug FAR*" },
+      --   -- pattern = { "grug-far" },
+      --   callback = function()
+      --     vim.keymap.set("n", "<leader>j", function()
+      --       vim.notify("hi")
+      --       -- require("grug-far").get_instance(0):open_location()
+      --       -- require("grug-far").get_instance(0):close()
+      --     end, { buffer = true })
+      --   end,
+      -- })
+    end)
+
+    vim.keymap.set("n", "<leader>l/", function()
+      grug_far.open({
+        prefills = { paths = vim.fn.expand("%") },
+      })
       -- vim.api.nvim_create_autocmd("FileType", {
       --   group = vim.api.nvim_create_augroup("grug-far-keybindings", { clear = true }),
       --   pattern = { "*Grug FAR*" },
